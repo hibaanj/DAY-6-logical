cfbe46a88700a44302f93d1806abfc713aeec98b
hibaanj <anjumhiba15@gmail.com>
2022-07-18 04:17:53 +0530

    numbers for a new coupon code

---
+++ UC5_couponnumber/UC5_couponnumber/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Logic
+{
+    
+    public int random()            
+        
+    {
+    
+        
+        Random rn = new Random();
+        int r = rn.Next(1, 10);
+        return r;
+    }
+    int i = 0;
+
+    public void distinct(int n)          
+    {
+        int[] a = new int[n];
+        for(i=1; i<n; i++)
+        {
+            a[i] = random();
+            for (int j = 0; j < i; j++)
+            {
+                if ((a[i] == n)&&( a[i] == a[j] ))
+                {
+                    a[i] = random();
+                    i--;
+                    break;
+                }
+            }
+        }
+        for(i=0; i<n; i++)
+        {
+            Console.WriteLine("the new discting number for coupon are ");
+            Console.WriteLine(a[i]);
+        }
+        
+
+    }
+    public static void Main(string[] args)
+    {
+        Logic l = new Logic();
+        int n = Convert.ToInt32(Console.ReadLine());
+        l.distinct(n);
+
+    }
+}
