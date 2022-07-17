53e7051204d9d6600ba3b46200d5885b813a79b2
hibaanj <anjumhiba15@gmail.com>
2022-07-18 02:31:01 +0530

    findign whether the number is prime or not

---
+++ UC3_Primenumbers/UC3_Primenumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Prime
+{
+    public static void Primnum()
+    {
+        Console.WriteLine("Enter a number");
+        int n = Convert.ToInt16(Console.ReadLine());
+        int a = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                a++;
+            }
+        }
+        if (a == 2)
+        {
+            Console.WriteLine($"{n} is a Prime Number");
+        }
+        else
+        {
+            Console.WriteLine($"{n} is Not a Prime Number");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Primnum();
+    }
+}
