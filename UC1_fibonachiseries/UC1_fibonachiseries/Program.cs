bdb1d5efef46b9a5e87c03191999231050f49950
hibaanj <anjumhiba15@gmail.com>
2022-07-18 02:09:55 +0530

    fibonachi series

---
+++ UC1_fibonachiseries/UC1_fibonachiseries/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// C# program for Fibonacci Series
+using System;
+
+public class Fibonachi
+{
+    public static int Fibo(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibo(n - 1) + Fibo(n - 2);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write(Fibo(n));
+    }
+}
