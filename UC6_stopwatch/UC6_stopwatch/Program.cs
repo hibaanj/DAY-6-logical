38c76895077bd49ec5f66c06a106a0e9595c9262
hibaanj <anjumhiba15@gmail.com>
2022-07-18 04:51:17 +0530

    stopwatch problem

---
+++ UC6_stopwatch/UC6_stopwatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+public class Stimer
+{
+    public static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        Console.WriteLine("write 1 to start");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        if (n == 1)
+        {
+            stopwatch.Start();
+            Console.WriteLine("write 0 to stop your clock");
+            int k = Convert.ToInt32(Console.ReadLine());
+            if (k == 0)
+            {
+                stopwatch.Stop();
+                Console.WriteLine("clock stopped");
+            }
+            Console.WriteLine("the elapsed time is "+stopwatch.Elapsed);
+
+            
+
+        }
+        else
+        {
+            Console.WriteLine("couldnt start watch");
+        }
+    }
+}
