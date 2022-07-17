acd56a8e9377472dd2a97034de0e1c020a70164b
hibaanj <anjumhiba15@gmail.com>
2022-07-18 02:18:31 +0530

    perfect number problem

---
+++ UC2_perfectnumber/UC2_perfectnumber/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Perfectnumber
+{
+
+    // Returns true if n is perfect
+    static string Perfect(int n)
+    {
+        // To store sum of divisors
+        int sum = 1;
+
+        // Find all divisors and add them
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                if (i * i != n)
+                    sum = sum + i + n / i;
+                else
+                    sum = sum + i;
+            }
+        }
+        
+        if (sum == n && n != 1)
+            return "is a perfect number";
+
+        return "is not a perfect number";
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Perfect( n);
+        Console.WriteLine(Perfect(n));
+    }
+}
